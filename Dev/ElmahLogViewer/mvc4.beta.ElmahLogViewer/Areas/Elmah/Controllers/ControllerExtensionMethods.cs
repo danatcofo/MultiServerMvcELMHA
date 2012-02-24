8d4d4897dce8c00918d62b6d391f16f352ae8936
danatcofo_cp <SND\danatcofo_cp>
2012-02-24 05:30:24 +0000

    Upgrade to MVC 4 Beta.  Integrated bundling with needed files.  Went back to using the Dynamic Query project as well rather than a nuget package.

---
+++ Dev/ElmahLogViewer/mvc4.beta.ElmahLogViewer/Areas/Elmah/Controllers/ControllerExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.Helpers;
+
+namespace ElmahLogViewer.Elmah.Models
+{
+    public static class ControllerExtensionMethods
+    {
+        public static IQueryable<TResult> GetQueryResults<T, TResult>(this IQueryable<T> v,
+            int startIndex,
+            int perPage,
+            string sort,
+            SortDirection sortDir,
+            System.Linq.Expressions.Expression<System.Func<T, TResult>> selector)
+        {
+            return v
+                .OrderBy(string.Format("{0} {1}", sort, sortDir))
+                .Skip(startIndex).Take(perPage)
+                .Select(selector);
+        }
+    }
+}
---
+++ Dev/ElmahLogViewer/mvc4.beta.ElmahLogViewer/Areas/Elmah/Models/Log/ServerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ElmahLogViewer.Elmah.Data;
+
+namespace ElmahLogViewer.Elmah.Models
+{
+    public class ServerForm : IServer
+    {
+        [Required]
+        public string ConnectionString { get; set; }
+
+        [Required]
+        public string Environment { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public Guid ServerId { get; set; }
+    }
+}
