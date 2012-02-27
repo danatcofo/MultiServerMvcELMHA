4c83fbfb9cb6ca1e050651c253ce69394b63064f
Daniel Gidman <danatcofo@users.noreply.github.com>
2012-02-27 16:04:10 +0000

    Merge to main

---
+++ mvc4.beta.ElmahLogViewer/Areas/Elmah/Models/Log/Lists.cs
@@ -0,0 +1,11 @@
+﻿using ElmahLogViewer.Elmah.Data;
+
+namespace ElmahLogViewer.Elmah.Models
+{
+    public class ServerList : ResultSet<IServer> { }
+
+    public class ErrorList : ResultSet<IELMAH_Error>
+    {
+        public IServer Server { get; set; }
+    }
+}
---
+++ mvc4.dev.ElmahLogViewer/Scripts/core.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-1.7.1.js"/>
+/// <reference path="jquery-ui-1.8.16.js"/>
+/// <reference path="jquery-validate.js"/>
+$(function () {
+    $(".datefield").datepicker();
+});
