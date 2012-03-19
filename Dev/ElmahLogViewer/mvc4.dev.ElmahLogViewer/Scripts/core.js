f6fc9d8f1065ba008af6d9497d1eaa7091a8a398
danatcofo_cp <SND\danatcofo_cp>
2012-03-19 18:32:43 +0000

    Clean up of old code.  Discontinued code

--- Dev/ElmahLogViewer/mvc4.dev.ElmahLogViewer/Scripts/core.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="jquery-1.7.1.js"/>
-/// <reference path="jquery-ui-1.8.16.js"/>
-/// <reference path="jquery-validate.js"/>
-$(function () {
-    $(".datefield").datepicker();
-});
