749316d828807ec51e02e62954dfe990fb68cb53
Daniel Gidman <danatcofo@users.noreply.github.com>
2012-03-29 14:57:56 +0000

    Mobile Solution w/ Views updated

---
+++ mvc4.beta.mobile.ElmahLogViewer/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System
+{
+    public static class ExtensionMethods
+    {
+        public static T Chain<T>(this T source, Action<T> action)
+        {
+            action(source);
+            return source;
+        }
+    }
+}
